4ccc144d056451594ec6bdee5cfd8882ddfbb54a
costadoat@crans.org <costadoat@crans.org>
2021-01-14 11:48:03 +0100

    Script ane pré

--- TD/TD03 L'âne est dans le pré/Code/I-TD03.py
+++ TD/TD03 L'âne est dans le pré/Code/I-TD03.py
@@ -1,102 +1,78 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 14 08:06:34 2016
-
-@author: Renaud
-"""
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-long=200
-larg=200
-corde=20
-pre=np.zeros((long, larg),'i')
+n_larg=200
+n_long=200
+c=20
 
+pre=np.zeros([n_larg,n_long])
 def manger(x,y):
-    for i in range(long):
-        for j in range(larg):
-            if (i-x)**2+(j-y)**2<corde**2 and pre[i,j]!=0:
-                pre[i,j]=2
-            if (i-x)**2+(j-y)**2<corde**2 and pre[i,j]==0:
-                pre[i,j]=1
-    return pre
+    for i in range(n_larg):
+        for j in range(n_long):
+            if ((x-i)**2+(y-j)**2)**(1/2.)<=c:
+                if pre[i,j]<2:
+                    pre[i,j]+=1
 
-def analyse(pre):
-    k,h=0,0    
-    x1,y1,x2,y2 = [],[],[],[]
-    for i in range(long):
-        for j in range(larg):
-            if pre[i,j] == 1:
-                k=k+1
+def parcours(e):
+    x,y=c,c
+    while x<n_larg-c:
+        while y<n_long-c:
+            manger(x,y)
+            y+=e
+        x+=e
+        y=c
+
+def parcours_q(e):
+    x,y=c,c
+    while x<n_larg-c:
+        while y<n_long-c:
+            manger(x,y)
+            y+=e
+        x+=e/2
+        if ((x-c)//(e/2))%2==0:
+            y=c
+        else:
+            y=c+e/2
+
+def trace(x1,y1,x2,y2):
+    plt.plot([0,n_larg,n_larg,0,0],[0,0,n_long,n_long,0],color='pink')
+    plt.scatter(x1,y1,s=1,color='green')
+    plt.scatter(x2,y2,s=1,color='red')
+    plt.axis('equal')
+    plt.xlim(0, n_long)
+    plt.ylim(0, n_larg)
+    plt.show()
+
+def pourcent(k,x1,x2):
+    if k==1:
+        return len(x1)/(n_long*n_larg)
+    if k==2:
+        return len(x2)/(n_long*n_larg)
+
+def analyse(yes_plot):
+    x1=[]
+    y1=[]
+    x2=[]
+    y2=[]
+    for i,ligne in enumerate(pre):
+        for j,valeur in enumerate(ligne):
+            if valeur==1.0:
                 x1.append(i)
                 y1.append(j)
-            if pre[i,j] == 2:
-                h=h+1
+            elif valeur==2.0:
                 x2.append(i)
                 y2.append(j)
-    return x1,y1,x2,y2,k,h
+    if yes_plot:
+        trace(x1,y1,x2,y2)
+    return 1-pourcent(1,x1,x2)-pourcent(2,x1,x2)
 
-def pourcent(k):
-    return 100*(k)/(long*larg)
 
-def calcul_classique(x0,y0,inc):
-    x=x0
-    y=y0
-    while y+corde < larg:
-        while x+corde < long:
-            pre=manger(x,y)
-            x=x+inc
-        x=x0
-        y=y+inc
-    return pre
+for e in np.arange(45,45,0.1):
+    pre=np.zeros([n_larg,n_long])
+    parcours_q(e)
+    print(e,int((n_larg-2*c)/e)+1,analyse(0))
 
-def calcul_quinconce(x0,y0,inc):
-    u=0
-    x=x0
-    y=y0
-    while y+corde+inc/2 < larg:
-        while x+corde < long:
-            u=u+1
-            if (u % 2 == 0):
-                pre=manger(x,y+inc/2)
-            else:
-                pre=manger(x,y)
-            x=x+inc
-        x=x0
-        u=0
-        y=y+inc
-    return pre
-
-pre=calcul_classique(20,20,35)
-#pre=calcul_quinconce(20,20,34.9)
-
-##########Recuperer la matrice si pas trouvee #####################
-fichier = open('D:\Documents\Dorian\Prepa\Informatique\TD03\Pre.txt','w')
-
-for i in range(long):
-    for j in range(larg):
-        fichier.write('%s,%s,%s\n' % (i,j,pre[i,j]))
-fichier.close()
-
-fichier = open('D:\Documents\Dorian\Prepa\Informatique\TD03\Pre.txt','r')
-
-for ligne in fichier: # on lit ligne par ligne
-    liste=ligne.split(',')
-    i,j=liste[0],liste[1]
-    pre[i,j]=liste[2]
-
-fichier.close() 
-####################################################################
-
-x1,y1,x2,y2,k,h=analyse(pre)
-print'Surface couverte %s %%' % pourcent(k+h)
-print'Surface couverte 2 fois %s %%' % pourcent(h)
-
-plt.scatter(x1,y1,s=1,color='green')
-plt.scatter(x2,y2,s=1,color='red')
-plt.axis('equal')
-plt.xlim(0, long)
-plt.ylim(0, larg)
-plt.show()
+e=45.5
+pre=np.zeros([n_larg,n_long])
+parcours_q(e)
+print(e,int((n_larg-2*c)/e)+1,analyse(1))
