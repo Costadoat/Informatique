66e64d86b0a616d8892ef7b97328ab3cb13d53b0
costadoat <costadoat@crans.org>
2023-12-15 16:14:16 +0100

    Info

--- DS/2022-2023/DS02/DS02.py
+++ DS/2022-2023/DS02/DS02.py
@@ -24,9 +24,10 @@ print(creneaux)
 compteurs=[]
 for ligne in lignes[1:-1]:
     data=ligne.split(';')
-    if [data[0],data[1],data[7],0] not in compteurs:
-        compteurs.append([data[0],data[1],[float(d) for d in data[7].split(',')],0])
-
+    data_compteur=[data[0],data[1],[float(d) for d in data[7].split(',')],0]
+    if data_compteur not in compteurs:
+        compteurs.append(data_compteur)
+        
 # Question 4:
 print(len(compteurs))
 
